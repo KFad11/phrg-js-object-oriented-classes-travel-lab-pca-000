b1e03c6da43b6b4c6cfd23507abb9bf173231f0e
KFad <kfadigan@yahoo.com>
2019-06-19 22:16:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return year - `${this.startDate.getFullYear()}`;
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  givenArray(avenue){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return eastWest.indexOf(avenue)
+  }
+
+  blocksTravelled(){
+    let horizontal = this.givenArray(this.endingLocation.horizontal) - this.givenArray(this.beginningLocation.horizontal)
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical
+    return horizontal + vertical
+  }
+
+  estimatedTime(peakHour){
+    if (peakHour) {
+      return this.blocksTravelled() / 2
+    }
+    else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
